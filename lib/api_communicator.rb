5e50f58beeecc813c4f00abfc08c39a97a82279d
vibraniumforge <gregm9999@gmail.com>
2019-09-10 20:56:36 -0700

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,7 +7,7 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
+  # iterate over the response hash to f ind the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
